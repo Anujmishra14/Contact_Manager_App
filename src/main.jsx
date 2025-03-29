8ef0b414015e73a9206421c664374967cb2aa26a
Anuj Mishra <133397372+Anujmishra14@users.noreply.github.com>
2025-03-29 23:40:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app.css";
+import "./  App.css";
 import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider,} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home.jsx";
